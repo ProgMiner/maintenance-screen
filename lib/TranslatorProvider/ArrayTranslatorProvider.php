379e718f4c78bc3e86e19d28dd1d2447bcd40b94
ProgMiner <eridan200@mail.ru>
2018-02-27 02:14:16 +0300

    Fixed namespaces

--- lib/TranslatorProvider/ArrayTranslatorProvider.php
+++ lib/TranslatorProvider/ArrayTranslatorProvider.php
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
-namespace MaintenanceScreen;
+namespace MaintenanceScreen\TranslatorProvider;
 
 /**
  * Array-based Translator instances provider
